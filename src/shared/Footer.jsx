a57b02ff55acd7897ba9dae84578d8b3c5c3a48e
DevAnikRoy <anikroy302@gmail.com>
2025-04-29 02:18:26 +0600

    adding the logo

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,14 +1,17 @@
 import { FaLinkedin, FaTwitter, FaEnvelope } from "react-icons/fa";
+import FooterImage from '../assets/footer-image.png'
 
 export default function Footer() {
   return (
     <footer className="bg-[#1A2238] text-white px-6 md:px-20 py-14">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-10">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 justify-center items-center gap-10">
+        {/* footer logo */}
+
         {/* Brand */}
-        <div>
-          <h2 className="text-2xl font-serif text-white mb-2">
-            ThinkBeyondAds
-          </h2>
+        <div className="flex flex-col justify-center items-start">
+          <div>
+            <img className="w-30 h-30 object-cover -mt-10 -ml-4" src={FooterImage} alt="" />
+          </div>
           <p className="text-sm text-gray-300 max-w-xs leading-relaxed">
             We engineer full-funnel growth systems that help high-LTV brands
             scale with strategy, not guesswork. Ads are just the beginning.
@@ -16,26 +19,28 @@ export default function Footer() {
         </div>
 
         {/* Navigation */}
-        <div className="flex flex-col space-y-3">
+        <div className="flex flex-col justify-between items-center space-y-3">
           <h3 className="text-sm font-semibold text-gray-400 uppercase tracking-wider">
             Quick Links
           </h3>
-          <a href="/services" className="hover:text-teal-300 text-sm">
-            Services
-          </a>
-          <a href="/case-studies" className="hover:text-teal-300 text-sm">
-            Case Studies
-          </a>
-          <a href="/about" className="hover:text-teal-300 text-sm">
-            About Us
-          </a>
-          <a href="/contact" className="hover:text-teal-300 text-sm">
-            Contact
-          </a>
+          <div className="flex flex-col justify-center items-start space-y-4">
+            <a href="/services" className="hover:text-teal-300 text-sm">
+              Services
+            </a>
+            <a href="/case-studies" className="hover:text-teal-300 text-sm">
+              Case Studies
+            </a>
+            <a href="/about" className="hover:text-teal-300 text-sm">
+              About Us
+            </a>
+            <a href="/contact" className="hover:text-teal-300 text-sm">
+              Contact
+            </a>
+          </div>
         </div>
 
         {/* CTA */}
-        <div>
+        <div className="flex flex-col">
           <h3 className="text-sm font-semibold text-gray-400 uppercase tracking-wider mb-4">
             Ready to Scale?
           </h3>
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -5,16 +5,18 @@ export default function Navbar() {
   return (
     <div className="relative">
       {/* Sticky Navbar */}
-      <nav className="fixed top-0 left-0 w-full bg-white shadow-md z-50 py-4 px-6 flex justify-between items-center">
-        <div className="text-xl font-serif text-[#023E8A]">ThinkBeyondAds</div>
-        <ul className="hidden md:flex space-x-6 font-sans text-sm text-gray-700">
-          <NavLink to="/" className="hover:text-[#0096C7] cursor-pointer">Home</NavLink>
-          <NavLink to="/services" className="hover:text-[#0096C7] cursor-pointer">Services</NavLink>
-          <NavLink className="hover:text-[#0096C7] cursor-pointer">Work</NavLink>
-          <NavLink to="/about" className="hover:text-[#0096C7] cursor-pointer">About</NavLink>
-          <NavLink to="/contact" className="hover:text-[#0096C7] cursor-pointer">Contact</NavLink>
+      <nav className="fixed top-0 left-0 w-full bg-white shadow-md z-50  px-6 flex justify-between items-center">
+        <div>
+          <img className='w-20 h-20' src="/src/assets/1.png" alt="" />
+        </div>
+        <ul className="hidden md:flex space-x-8 font-sans text-sm text-gray-700">
+          <NavLink to="/" className="hover:text-[#0096C7] text-[1rem] cursor-pointer">Home</NavLink>
+          <NavLink to="/services" className="hover:text-[#0096C7] text-[1rem] cursor-pointer">Services</NavLink>
+          <NavLink className="hover:text-[#0096C7] text-[1rem] cursor-pointer">Work</NavLink>
+          <NavLink to="/about" className="hover:text-[#0096C7] text-[1rem] cursor-pointer">About</NavLink>
+          <NavLink to="/contact" className="hover:text-[#0096C7] text-[1rem] cursor-pointer">Contact</NavLink>
         </ul>
-        <button className="bg-[#023E8A] text-white px-4 py-2 rounded font-bold uppercase text-xs shadow hover:bg-[#0096C7] transition">
+        <button className="bg-[#0096C7] text-white px-4 py-2 rounded font-bold uppercase text-xs shadow hover:bg-[#023E8A] transition">
           Book Call
         </button>
       </nav>
