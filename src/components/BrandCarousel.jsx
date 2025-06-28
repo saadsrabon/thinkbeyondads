82bdf99ee6cfd901046176fc2001e3d0e38b4bf5
Saad Srabon <saadsrabon2@gmail.com>
2025-06-28 23:50:35 +0600

    added 15 logo

--- src/components/BrandCarousel.jsx
+++ src/components/BrandCarousel.jsx
@@ -5,6 +5,15 @@ import logo3 from '../assets/clientlogo/logo (3).png';
 import logo4 from '../assets/clientlogo/logo (4).png';
 import logo5 from '../assets/clientlogo/logo (5).png';
 import logo6 from '../assets/clientlogo/logo (6).webp';
+import logo7 from '../assets/clientlogo/logo7.avif';
+import logo8 from '../assets/clientlogo/logo8.png';
+import logo9 from '../assets/clientlogo/logo9.png';
+import logo10 from '../assets/clientlogo/logo10.jpg';
+import logo11 from '../assets/clientlogo/logo11.png';
+import logo12 from '../assets/clientlogo/logo12.jpg';
+import logo13 from '../assets/clientlogo/logo13.jpg';
+import logo14 from '../assets/clientlogo/logo14.jpg';
+
 const logos = [
     logo,
     logo2,
@@ -12,6 +21,14 @@ const logos = [
     logo4,
     logo5,
     logo6,
+    logo7,
+    logo8,
+    logo9,
+    logo10,
+    logo11,
+    logo12,
+    logo13,
+    logo14,
     // Add more logos as needed
 ];
 
--- src/components/TrustSection.jsx
+++ src/components/TrustSection.jsx
@@ -6,7 +6,7 @@ export default function TrustSection() {
   return (
     <section className="  text-white py-16">
       <div className="text-center px-4">
-        <h2 className="text-4xl md:text-5xl font-serif font-bold text-gray-900 dark:text-white mb-6">
+        <h2 className="text-2xl md:text-4xl font-serif font-bold text-gray-900 dark:text-white mb-6">
           Trusted by 300+ leading brands in <br />
           only 5 years – see why.
         </h2>
