c6f0675aeb0f864c0ce71db8191a7aa971faa3b3
Saad Srabon <saadsrabon2@gmail.com>
2025-06-30 01:46:26 +0600

    Transparent Logo added

--- src/components/TrustSection.jsx
+++ src/components/TrustSection.jsx
@@ -4,9 +4,9 @@ import TestimonialsSlider from "./TestimonialSlider";
 
 export default function TrustSection() {
   return (
-    <section className="  text-white py-16">
+    <section className="  text-white py-16 ">
       <div className="text-center px-4">
-        <h2 className="text-2xl md:text-4xl font-serif font-bold text-gray-900 dark:text-white mb-6">
+        <h2 className="text-2xl md:text-4xl font-serif font-bold text-bg-accent mb-6">
           Trusted by 300+ leading brands in <br />
           only 5 years – see why.
         </h2>
