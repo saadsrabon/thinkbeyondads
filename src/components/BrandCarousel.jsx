d3644530e682036515ba6f755bc96e5e479110b6
Saad Srabon <saadsrabon2@gmail.com>
2025-07-03 22:02:24 +0600

    files uploaded

--- src/components/BrandCarousel.jsx
+++ src/components/BrandCarousel.jsx
@@ -1,18 +1,18 @@
 import { motion } from "framer-motion";
-import logo from "../assets/clientlogo/logo (1).png";
-import logo2 from "../assets/clientlogo/logo (2).png";
-import logo3 from "../assets/clientlogo/logo (3).png";
-import logo4 from "../assets/clientlogo/logo (4).png";
-import logo5 from "../assets/clientlogo/logo (5).png";
+import logo from "../assets/clientlogo/logo (1).webp";
+import logo2 from "../assets/clientlogo/logo (2).webp";
+import logo3 from "../assets/clientlogo/logo (3).webp";
+import logo4 from "../assets/clientlogo/logo (4).webp";
+import logo5 from "../assets/clientlogo/logo (5).webp";
 import logo6 from "../assets/clientlogo/logo (6).webp";
-import logo7 from "../assets/clientlogo/logo7.avif";
+import logo7 from "../assets/clientlogo/logo7.webp";
 import logo8 from "../assets/clientlogo/logo8.png";
-import logo9 from "../assets/clientlogo/logo9.png";
-import logo10 from "../assets/clientlogo/logo10.jpg";
-import logo11 from "../assets/clientlogo/logo11.png";
-import logo12 from "../assets/clientlogo/logo12.jpg";
-import logo13 from "../assets/clientlogo/logo13.jpg";
-import logo14 from "../assets/clientlogo/logo14.jpg";
+import logo9 from "../assets/clientlogo/logo9.webp";
+import logo10 from "../assets/clientlogo/logo10.webp";
+import logo11 from "../assets/clientlogo/logo11.webp";
+import logo12 from "../assets/clientlogo/logo12.webp";
+import logo13 from "../assets/clientlogo/logo13.webp";
+import logo14 from "../assets/clientlogo/logo14.webp";
 
 const logos = [
   logo,
